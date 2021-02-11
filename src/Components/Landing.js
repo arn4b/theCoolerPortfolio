96f68489edebe18c655d4b69ff2bf3045fcd74d3
Arnab Chatterjee <arnabc1701@gmail.com>
2021-02-11 15:21:24 +0530

    Landing Skeleton designed

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -7,7 +7,20 @@ class Landing extends Component {
         return(
             <header id="home">
                 <ParticlesBg color="#d400ff" type="cobweb" bg = {true} />
-                
+                //~~~~~~~~~~~code for navbar~~~~~~~~~~~
+                <div className="landing-text">
+                    <h1>Hello, <br/>I'm Arnab Chatterjee  </h1><br/>
+                    <h2>
+                    It was <span
+                        class="typer"
+                        id="some-id"
+                        data-words="dark.,stormy.,night."
+                        data-delay="200"
+                        data-colors="#fff, #d400ff">
+                    </span>
+                    <span  class="cursor"  data-owner="some-id"></span>
+                    </h2>
+                </div>
             </header>
 
         );

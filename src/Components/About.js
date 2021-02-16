28092c654d0565bc5a04e46971617f0418f70c4b
Arnab Chatterjee <arnabc1701@gmail.com>
2021-02-16 13:43:29 +0530

    minor changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,6 +9,9 @@ class About extends Component {
                 <div className="aboutme">
                     <h1>About Me</h1>
                     <h3>I'm a 19 y/o Student and a Full Stack Web Developer currently working with GeeksForGeeks KIIT as a frontend and backend developer. As a Web Developer, I mostly work with React and NodeJS, and multiple other JavaScript frameworks which aid in making highly responsive web pages. I'm also a bot developer, having developed bots for Discord, Twitter and Chatbots.</h3>
+                    <p className="button-p">
+                        <a href="https://drive.google.com/file/d/1KEneviCg2I6aijEuwHUc6hyFlCNKEWbV/view?usp=sharing"  className="button">Get my Resume</a>
+                    </p>
                 </div>
             </section>
         );

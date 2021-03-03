34960c693b545d79a3fd2726c92e40b78b98752e
Arnab Chatterjee <arnabc1701@gmail.com>
2021-03-03 12:41:42 +0530

    projects hover animation added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,7 +5,35 @@ class Projects extends Component {
         return(
             <section id="projects">
                 <h1>My Work</h1>
+                <div className="row">
+                    <div className="double-column skill">
+                        <div className="container">
+                            <a href="https://github.com/arn4b/instagram-clone">
+                                <img src="https://wallpapercave.com/wp/wp2587127.jpg" className="pimg" />
+                                <div className="middle">
+                                
+                                <div className="text">Full Stack Instagram Clone</div>
+                                <div className="text"><i class="fab fa-github"></i></div>
+                                
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
+                    <div className="double-column skill">
+                        <div className="container">
+                            <a href="https://github.com/arn4b/superchat">
+                                <img src="https://wallpapercave.com/wp/wp2587127.jpg" className="pimg" />
+                                <div className="middle">
+                                
+                                <div className="text">Firebase based Chatroom</div>
+                                <div className="text"><i class="fab fa-github"></i>&nbsp;GitHub</div>
+                                
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 
             </section>
         );

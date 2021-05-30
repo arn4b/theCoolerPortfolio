007dc19ed1e3bd76d6792abefc3ab312911b69f2
Arnab Chatterjee <arnabc1701@gmail.com>
2021-05-31 01:51:55 +0530

    css tweaks

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,19 +10,19 @@ class About extends Component {
                 <div className="row">
 
                     <div className="double-column">
-                        <p>I'm a Junior at Kalinga Institute of Industrial Technology, Bhubaneshwar, majoring in Computer Science. 
-                            I have a keen interest in developing mobile-first fully responsive frontend sites using ReactJS, and multiple frontend 
-                            frameworks like Bootstrap, MaterialUI, etc. I have worked on multiple projects using React, 
-                            NodeJS, Firebase and multiple public APIs. Currently, I'm working as a Web Developer at 
-                            GFG KIIT Chapter and MSAC KIIT. I am open to work and collaborate on open source projects, 
-                            Frontend projects, and UI designing.</p><br/>
-                        <p className="button-p">
-                            <a href="https://drive.google.com/file/d/1vrIq3-ZjOXmfHcn9i91R8wj7vBhJNY2I/view?usp=sharing"  className="button"><i class="fas fa-download"></i> Get my Resume</a>
-                        </p>
+                        <p>I'm a Junior at Kalinga Institute of Industrial Technology, Bhubaneshwar, majoring in Computer Science and System Engineering.<br/><br/>
+                            I have over 1 and a half years of experience in developing mobile-first fully responsive frontend sites using ReactJS, and multiple frontend 
+                            frameworks like Bootstrap, MaterialUI, etc. as well as developing mobile apps for both Android and iOS using React Native. I have worked on multiple projects using React, 
+                            NodeJS, Firebase and multiple other JS frameworks. <br/><br/>Currently, I'm working as a Web Developer at 
+                            GFG KIIT Chapter, Microsoft Student Ambassador Community, Hackclub and Mozilla BBSR. I am open to work and collaborate on 
+                            Frontend projects, Native mobile apps and UI designing.</p><br/>
                     </div>
 
                     <div className="column">
                         <img className="profilepic" src={profilepic}/>
+                        <p className="button-p">
+                            <a href="https://drive.google.com/file/d/1vrIq3-ZjOXmfHcn9i91R8wj7vBhJNY2I/view?usp=sharing"   target="_blank" className="button"><i class="fas fa-download"></i> Get my Resume</a>
+                        </p>
                     </div>
                 </div>
 
@@ -34,7 +34,7 @@ class About extends Component {
                             Check out my repos at
                         </h3>
                         <p className="button-p1">
-                            <a href="https://github.com/arn4b"  className="button1"><i className="fab fa-github"></i> &nbsp;GitHub</a>
+                            <a href="https://github.com/arn4b" target="_blank"  className="button1"><i className="fab fa-github"></i> &nbsp;GitHub</a>
                         </p>
                     </div>
 
@@ -43,7 +43,7 @@ class About extends Component {
                             Connect with me on
                         </h3>
                         <p className="button-p1">
-                            <a href="https://www.linkedin.com/in/arnab-chatterjee-3944341a0/"  className="button1"><i className="fab fa-linkedin-in"></i>&nbsp; LinkedIn</a>
+                            <a href="https://www.linkedin.com/in/arnab-chatterjee-3944341a0/"  target="_blank"  className="button1"><i className="fab fa-linkedin-in"></i>&nbsp; LinkedIn</a>
                         </p>
                     </div>
 
@@ -52,7 +52,7 @@ class About extends Component {
                             Follow me on
                         </h3>
                         <p className="button-p1">
-                            <a href="https://instagram.com/_arn4b_"  className="button1"><i className="fab fa-instagram"></i>&nbsp; Instagram</a>
+                            <a href="https://instagram.com/_arn4b_"  target="_blank"  className="button1"><i className="fab fa-instagram"></i>&nbsp; Instagram</a>
                         </p>
                     </div>
                 </div>
